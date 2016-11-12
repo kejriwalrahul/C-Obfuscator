9d7963b678b2c51db2762e40f24dc926babb2797
Rahul Kejriwal <kejriwalrahul@outlook.com>
2016-11-12 19:38:26 +0530

    begin layer 1

--- Layer 1/ParserMain.java
+++ Layer 1/ParserMain.java
@@ -6,7 +6,7 @@ public class ParserMain {
       try {
          Node root = new CParser(System.in).Goal();
          System.out.println("Program parsed successfully");
-         // root.accept(new GJDepthFirst<Integer,String>(),null); // Your assignment part is invoked here.
+         root.accept(new GJDepthFirst<Integer,String>(),null); // Your assignment part is invoked here.
       }
       catch (ParseException e) {
          System.out.println(e.toString());
